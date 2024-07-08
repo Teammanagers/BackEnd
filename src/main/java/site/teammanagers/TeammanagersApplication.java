ea30fd8929708f3a49a244b18e280eca764ea5eb
Luke_YS <yjsmk0902@gmail.com>
2024-07-08 19:18:55 +0900

    Init: Set up

---
+++ src/main/java/site/teammanagers/TeammanagersApplication.java
@@ -0,0 +1,13 @@
+package site.teammanagers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeammanagersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeammanagersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/teammanagers/TeammanagersApplicationTests.java
@@ -0,0 +1,13 @@
+package site.teammanagers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeammanagersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
