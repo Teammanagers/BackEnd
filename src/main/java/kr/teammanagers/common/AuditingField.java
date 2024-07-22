af239e1711ac146940f1cd8bc25e6ca5c659ca05
Luke_YS <yjsmk0902@gmail.com>
2024-07-22 20:38:01 +0900

    Feature: AuditingField 구현 및 설정(#4)

---
+++ src/main/java/kr/teammanagers/common/AuditingField.java
@@ -0,0 +1,29 @@
+package kr.teammanagers.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AuditingField {
+
+    @CreatedDate
+    @Column(updatable=false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Enumerated(value = EnumType.STRING)
+    private EntityStatus status = EntityStatus.ACTIVE;
+
+    public void updateStatus(final EntityStatus status){
+        this.status = status;
+    }
+}
