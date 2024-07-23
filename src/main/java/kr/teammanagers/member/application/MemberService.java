ed530cddf418919b9352b8761b09b7d0fa15a02d
Luke_YS <yjsmk0902@gmail.com>
2024-07-23 23:50:16 +0900

    Feature: 도메인 기본 골조 구현 (#7)

---
+++ src/main/java/kr/teammanagers/member/application/MemberService.java
@@ -0,0 +1,15 @@
+package kr.teammanagers.member.application;
+
+import kr.teammanagers.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+}
---
+++ src/main/java/kr/teammanagers/member/presentation/MemberController.java
@@ -0,0 +1,15 @@
+package kr.teammanagers.member.presentation;
+
+import kr.teammanagers.member.application.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+}
