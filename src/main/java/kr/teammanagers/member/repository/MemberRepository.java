718cd0825bf9f8111cde205806cd3de3437f3ad7
Boreum <itip1@mju.ac.kr>
2024-07-30 04:53:49 +0900

    [Feat] ProviderId add

--- src/main/java/kr/teammanagers/member/repository/MemberRepository.java
+++ src/main/java/kr/teammanagers/member/repository/MemberRepository.java
@@ -1,7 +1,11 @@
 package kr.teammanagers.member.repository;
 
 import kr.teammanagers.member.domain.Member;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberQueryDsl {
+    Optional<Member> findByProviderId(String providerId);
 }
