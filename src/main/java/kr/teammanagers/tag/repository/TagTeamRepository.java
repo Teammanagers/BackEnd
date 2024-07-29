04dab56a743f6cbd41726bfad7bb7a72ea187621
Luke_YS <yjsmk0902@gmail.com>
2024-07-29 18:48:51 +0900

    Feature: 태그 도메인 Repository 구현 (#7)

--- src/main/java/kr/teammanagers/tag/repository/TagTeamRepository.java
+++ src/main/java/kr/teammanagers/tag/repository/TagTeamRepository.java
@@ -4,8 +4,13 @@ import kr.teammanagers.tag.domain.TagTeam;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TagTeamRepository extends JpaRepository<TagTeam, Long> {
 
     List<TagTeam> findAllByTeamId(Long teamId);
+
+    Optional<TagTeam> findByTeamIdAndTagId(Long teamId, Long tagId);
+
+    Boolean existsByTagId(Long tagId);
 }
