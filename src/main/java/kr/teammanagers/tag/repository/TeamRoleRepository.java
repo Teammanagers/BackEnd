befacce6d72c25bf24db851c59a925b92dc53cc6
Luke_YS <52543606+yjsmk0902@users.noreply.github.com>
2024-07-30 01:21:16 +0900

    [Refactor] 사용자 도메인 구현 누락 로직 추가 (#14)
    * Refactor: Dto 팩토리 메서드 네이밍 수정 (#7)
    
    * Refactor: MemberService 코드 복잡도 개선 (#7)
    
    * Refactor: MemberController 쓰지 않는 파라미터 제거 (#7)
    
    * Feature: 태그 도메인 Repository 구현 (#7)
    
    * Refactor: TagModuleService 코드 복잡도 개선 (#7)
    
    * [Refactor] Exception 패키지 생성 및 파일 위치 변경 (#13)
    
    * Fix: Todo 도메인 Status enum 수정 (#7)

--- src/main/java/kr/teammanagers/tag/repository/TeamRoleRepository.java
+++ src/main/java/kr/teammanagers/tag/repository/TeamRoleRepository.java
@@ -1,11 +1,16 @@
-package kr.teammanagers.team.repository;
+package kr.teammanagers.tag.repository;
 
 import kr.teammanagers.tag.domain.TeamRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TeamRoleRepository extends JpaRepository<TeamRole, Long> {
 
     List<TeamRole> findAllByTeamManageId(Long teamManageId);
+
+    Optional<TeamRole> findByTeamManageIdAndTagId(Long teamManageId, Long tagId);
+
+    Boolean existsByTagId(Long tagId);
 }
