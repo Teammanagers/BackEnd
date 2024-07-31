cabaed126ea33bf5ba6e74b5f18f48a5f7fef48e
Luke_YS <yjsmk0902@gmail.com>
2024-08-01 03:07:02 +0900

    Refactor: TeamDto 파라미터 변경에 따른 로직 수정 (#26)

--- src/main/java/kr/teammanagers/team/dto/TeamDto.java
+++ src/main/java/kr/teammanagers/team/dto/TeamDto.java
@@ -1,5 +1,6 @@
 package kr.teammanagers.team.dto;
 
+import kr.teammanagers.tag.domain.Tag;
 import kr.teammanagers.tag.dto.TagDto;
 import kr.teammanagers.team.domain.Team;
 import lombok.Builder;
@@ -15,12 +16,15 @@ public record TeamDto(
         List<TagDto> teamTagList
 ) {
 
-    public static TeamDto from(final Team team) {
+    public static TeamDto from(final Team team, final List<Tag> tagList) {
         return TeamDto.builder()
                 .teamId(team.getId())
                 .title(team.getTitle())
                 .teamCode(team.getTeamCode())
                 .imageUrl(team.getTeamImageUrl())
+                .teamTagList(tagList.stream()
+                        .map(TagDto::from)
+                        .toList())
                 .build();
     }
 }
