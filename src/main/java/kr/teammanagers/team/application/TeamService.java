d5b0bfcb943f6bc3483ade6437e63941dda5fcfa
Luke_YS <yjsmk0902@gmail.com>
2024-08-01 03:13:18 +0900

    Feature: 팀 도메인 관련 기능 로직 구현 (#26)

---
+++ src/main/java/kr/teammanagers/team/application/TeamService.java
@@ -0,0 +1,162 @@
+package kr.teammanagers.team.application;
+
+import kr.teammanagers.common.Status;
+import kr.teammanagers.global.config.AmazonConfig;
+import kr.teammanagers.global.provider.AmazonS3Provider;
+import kr.teammanagers.member.domain.Comment;
+import kr.teammanagers.member.domain.Member;
+import kr.teammanagers.member.repository.CommentRepository;
+import kr.teammanagers.member.repository.MemberRepository;
+import kr.teammanagers.tag.application.TagModuleService;
+import kr.teammanagers.tag.domain.Tag;
+import kr.teammanagers.tag.domain.TagTeam;
+import kr.teammanagers.tag.domain.TeamRole;
+import kr.teammanagers.tag.repository.TagTeamRepository;
+import kr.teammanagers.tag.repository.TeamRoleRepository;
+import kr.teammanagers.team.domain.Team;
+import kr.teammanagers.team.domain.TeamManage;
+import kr.teammanagers.team.dto.SimpleTeamMemberDto;
+import kr.teammanagers.team.dto.TeamMemberDto;
+import kr.teammanagers.team.dto.request.CreateTeam;
+import kr.teammanagers.team.dto.request.CreateTeamComment;
+import kr.teammanagers.team.dto.request.CreateTeamPassword;
+import kr.teammanagers.team.dto.response.CreateTeamResult;
+import kr.teammanagers.team.dto.response.GetTeam;
+import kr.teammanagers.team.dto.response.GetTeamMember;
+import kr.teammanagers.team.dto.response.UpdateTeamEndResult;
+import kr.teammanagers.team.repository.TeamManageRepository;
+import kr.teammanagers.team.repository.TeamRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+import static kr.teammanagers.team.constant.TeamConstant.*;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class TeamService {
+
+    private final TeamRepository teamRepository;
+    private final MemberRepository memberRepository;
+    private final TagTeamRepository tagTeamRepository;
+    private final TeamManageRepository teamManageRepository;
+    private final TeamRoleRepository teamRoleRepository;
+
+    private final TagModuleService tagModuleService;
+    private final AmazonS3Provider amazonS3Provider;
+    private final AmazonConfig amazonConfig;
+    private final CommentRepository commentRepository;
+
+    @Transactional
+    public CreateTeamResult createTeam(final Long authId, final CreateTeam request, final MultipartFile imageFile) {
+        Member member = memberRepository.getReferenceById(authId);
+        String imageUrl = amazonS3Provider.uploadImage(
+                amazonS3Provider.generateKeyName(amazonConfig.getTeamProfilePath()), imageFile);
+        Team team = teamRepository.save(request.toTeam(imageUrl));
+        team.updateTeamCode(encodeNumberToChars(team.getId()));
+
+        request.teamTagList().stream()
+                .map(tagModuleService::findOrCreateTag)
+                .forEach(tag -> tagTeamRepository.save(
+                        TagTeam.builder()
+                                .tag(tag)
+                                .team(team)
+                                .build()
+                ));
+
+        TeamManage admin = TeamManage.builder()
+                .isDeleted(false)
+                .build();
+        admin.setTeam(team);
+        admin.setMember(member);
+        teamManageRepository.save(admin);
+        return CreateTeamResult.from(team);
+    }
+
+    @Transactional
+    public void createTeamPassword(final Long teamId, final CreateTeamPassword request) {
+        Team team = teamRepository.findById(teamId)
+                .orElseThrow(RuntimeException::new);        // TODO : 예외 처리 필요
+        team.updatePassword(request.password());
+    }
+
+    public GetTeam getTeamById(final Long teamId) {
+        return teamRepository.findById(teamId)
+                .map(team -> {
+                    List<Tag> tagList = tagTeamRepository.findAllByTeamId(team.getId()).stream()
+                            .map(TagTeam::getTag).toList();
+                    return GetTeam.from(team, tagList);
+                })
+                .orElseThrow(RuntimeException::new);        // TODO : 예외 처리 필요
+
+    }
+
+    public GetTeam getTeamByTeamCode(final String teamCode) {
+        return teamRepository.findByTeamCode(teamCode)
+                .map(team -> {
+                    List<Tag> tagList = tagTeamRepository.findAllByTeamId(team.getId()).stream()
+                            .map(TagTeam::getTag).toList();
+                    return GetTeam.from(team, tagList);
+                })
+                .orElse(null);
+    }
+
+    public GetTeamMember getTeamMember(final Long teamId) {
+        List<SimpleTeamMemberDto> memberList = teamManageRepository.findAllByTeamId(teamId).stream()
+                .map(SimpleTeamMemberDto::from)
+                .toList();
+
+        return GetTeamMember.from(memberList);
+    }
+
+    @Transactional
+    public UpdateTeamEndResult updateTeamState(final Long authId, final Long teamId) {
+        Team team = teamRepository.findById(teamId)
+                .orElseThrow(RuntimeException::new);        // TODO : 예외 처리 필요
+        team.updateStatus(Status.COMPLETED);
+        List<TeamMemberDto> teamMemberList = teamManageRepository.findAllByTeamId(teamId).stream()
+                .filter(teamManage -> !teamManage.getMember().getId().equals(authId))
+                .map(teamManage -> {
+                    List<Tag> tagList = teamRoleRepository.findAllByTeamManageId(teamManage.getId()).stream()
+                            .map(TeamRole::getTag).toList();
+                    return TeamMemberDto.of(teamManage, tagList);
+                }).toList();
+
+        return UpdateTeamEndResult.from(teamMemberList);
+    }
+
+    @Transactional
+    public void createComment(final CreateTeamComment request) {
+        request.commentList()
+                .forEach(registerCommentDto -> {
+                    Long memberId = teamManageRepository.findById(registerCommentDto.teamManageId())
+                            .orElseThrow(RuntimeException::new)     // TODO : 예외 처리 필요
+                            .getMember().getId();
+                    Comment comment = Comment.builder()
+                            .content(registerCommentDto.content())
+                            .isHidden(false)
+                            .build();
+                    comment.setMember(memberRepository.getReferenceById(memberId));
+                    commentRepository.save(comment);
+                });
+    }
+
+    private String encodeNumberToChars(final Long teamId) {
+        StringBuilder sb = new StringBuilder();
+        Long number = teamId;
+        while (number > 0) {
+            sb.append(BASE62.charAt((int) (number % BASE)));
+            number /= BASE;
+        }
+
+        while (sb.length() < PADDING) {
+            sb.append(BASE62.charAt(RANDOM.nextInt(BASE)));
+        }
+
+        return sb.reverse().toString();
+    }
+}
