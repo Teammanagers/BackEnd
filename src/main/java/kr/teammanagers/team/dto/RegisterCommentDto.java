26f4f5ae7aab0cdfd28c1bd25457a3dea43472ef
Luke_YS <yjsmk0902@gmail.com>
2024-08-05 15:55:42 +0900

    Feature: RegisterCommentDto 검증 어노테이션 추가 (#34)

--- src/main/java/kr/teammanagers/team/dto/RegisterCommentDto.java
+++ src/main/java/kr/teammanagers/team/dto/RegisterCommentDto.java
@@ -1,7 +1,14 @@
 package kr.teammanagers.team.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record RegisterCommentDto(
+        @NotNull
         Long teamManageId,
+
+        @NotNull
+        @Size(max = 50)
         String content
 ) {
 }
