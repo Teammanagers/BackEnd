1cc09adb7c5e465d03fa065d24c600ff0bf35297
Luke_YS <yjsmk0902@gmail.com>
2024-08-05 16:12:42 +0900

    Feature: CreateTeamPassword DTO 검증 어노테이션 추가 (#34)

--- src/main/java/kr/teammanagers/team/dto/request/CreateTeamPassword.java
+++ src/main/java/kr/teammanagers/team/dto/request/CreateTeamPassword.java
@@ -1,6 +1,9 @@
 package kr.teammanagers.team.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 public record CreateTeamPassword(
+        @Size(max = 6)
         String password
 ) {
 }
