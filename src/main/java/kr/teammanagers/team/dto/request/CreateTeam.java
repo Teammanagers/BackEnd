19a4379913116faa1b64e154bc48faad3995d07a
Luke_YS <yjsmk0902@gmail.com>
2024-08-05 15:55:16 +0900

    Feature: CreateTeam DTO 검증 어노테이션 추가 (#34)

--- src/main/java/kr/teammanagers/team/dto/request/CreateTeam.java
+++ src/main/java/kr/teammanagers/team/dto/request/CreateTeam.java
@@ -1,13 +1,18 @@
 package kr.teammanagers.team.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import kr.teammanagers.common.Status;
+import kr.teammanagers.tag.exception.TagMaxSize;
 import kr.teammanagers.team.constant.TeamConstant;
 import kr.teammanagers.team.domain.Team;
 
 import java.util.List;
 
 public record CreateTeam(
+        @NotNull
         String title,
+
+        @TagMaxSize
         List<String> teamTagList
 ) {
     public Team toTeam(final String imageUrl) {
