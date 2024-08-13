28cabb1f214e13a9f72286460aea38edb85dad0a
lheesoo1020 <lheesoo1020@gmail.com>
2024-08-13 20:04:13 +0900

    Refactor: TeamController CrossOrigin 어노테이션 수정

--- src/main/java/kr/teammanagers/team/presentation/TeamController.java
+++ src/main/java/kr/teammanagers/team/presentation/TeamController.java
@@ -20,7 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TeamController {
 
     private final TeamQueryService teamQueryService;
