eba036e630f8a51e1245805899dc52ea24add67f
lheesoo1020 <lheesoo1020@gmail.com>
2024-08-15 02:07:31 +0900

    Fix: WebConfig 수정2 (#57)

--- src/main/java/WebConfig.java
+++ src/main/java/WebConfig.java
@@ -5,15 +5,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173");
     }
 }
