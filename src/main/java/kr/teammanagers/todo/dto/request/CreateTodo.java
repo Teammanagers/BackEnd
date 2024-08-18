64815f41ae91c85a23c91f25e3ab580245a67efc
lheesoo1020 <lheesoo1020@gmail.com>
2024-08-18 16:47:44 +0900

    Feat: @Size message 추가 (#42)

--- src/main/java/kr/teammanagers/todo/dto/request/CreateTodo.java
+++ src/main/java/kr/teammanagers/todo/dto/request/CreateTodo.java
@@ -2,10 +2,11 @@ package kr.teammanagers.todo.dto.request;
 
 import jakarta.validation.constraints.Size;
 import kr.teammanagers.common.Status;
+import kr.teammanagers.common.payload.code.constant.ValidatorErrorConstant;
 import kr.teammanagers.todo.domain.Todo;
 
 public record CreateTodo(
-        @Size(max = 30)
+        @Size(max = 30, message = ValidatorErrorConstant.SIZE)
         String title
 ) {
     public Todo toTodo() {
--- src/main/java/kr/teammanagers/todo/dto/request/UpdateTodo.java
+++ src/main/java/kr/teammanagers/todo/dto/request/UpdateTodo.java
@@ -1,9 +1,10 @@
 package kr.teammanagers.todo.dto.request;
 
 import jakarta.validation.constraints.Size;
+import kr.teammanagers.common.payload.code.constant.ValidatorErrorConstant;
 
 public record UpdateTodo(
-        @Size(max = 30)
+        @Size(max = 30, message = ValidatorErrorConstant.SIZE)
         String title
 ) {
 }
