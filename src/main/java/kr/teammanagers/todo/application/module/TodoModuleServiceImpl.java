8225e2438050ddce70aac3542945ea807ff0d924
lheesoo1020 <lheesoo1020@gmail.com>
2024-08-20 20:57:01 +0900

    Fix: ModuleService @Transactional 제거

--- src/main/java/kr/teammanagers/todo/application/module/TodoModuleServiceImpl.java
+++ src/main/java/kr/teammanagers/todo/application/module/TodoModuleServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class TodoModuleServiceImpl implements TodoModuleService {
 
