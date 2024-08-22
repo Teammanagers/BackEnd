305a385d9f2276f9f14b3d217af8c8339e8b1ee9
Luke_YS <yjsmk0902@gmail.com>
2024-08-22 23:26:58 +0900

    Refactor: 투두 도메인 모듈서비스 계층 분리 적용 (#104)

--- src/main/java/kr/teammanagers/todo/application/module/TodoModuleService.java
+++ src/main/java/kr/teammanagers/todo/application/module/TodoModuleService.java
@@ -6,7 +6,12 @@ import java.util.List;
 
 public interface TodoModuleService {
     Todo getTodoById(Long id);
+
     List<Todo> getTodoListByTeamManageId(Long teamManageId);
+
     void saveTodo(Todo todo);
+
     void deleteTodoById(Long id);
+
+    void deleteAllByTeamManageId(Long teamManageId);
 }
