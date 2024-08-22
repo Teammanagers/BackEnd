a3a26a76c028d0c8cf5bdaa2e072fefd8ae3a1f0
지현수 <itip1@mju.ac.kr>
2024-08-22 22:58:53 +0900

    [feat] IsnewUser method add

--- src/main/java/kr/teammanagers/auth/handler/OAuth2SuccessHandler.java
+++ src/main/java/kr/teammanagers/auth/handler/OAuth2SuccessHandler.java
@@ -3,9 +3,9 @@ package kr.teammanagers.auth.handler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import kr.teammanagers.auth.dto.PrincipalDetails;
 import kr.teammanagers.global.provider.TokenProvider;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -23,6 +23,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
         String accessToken = tokenProvider.generateAccessToken(authentication);
         tokenProvider.generateRefreshToken(authentication, accessToken);
 
@@ -30,6 +31,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
         redirectUrl = UriComponentsBuilder.fromUriString(redirectUrl)
                 .queryParam("accessToken", accessToken)
+                .queryParam("isNewUser",principalDetails.isNewUser())
                 .build().toUriString();
 
         response.sendRedirect(redirectUrl);
