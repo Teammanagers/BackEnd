70599d65dd11852a11d5a57f63874f5849c7bf60
Luke_YS <yjsmk0902@gmail.com>
2024-08-22 16:52:05 +0900

    Feature: 에러 응답 추가 (#104)

--- src/main/java/kr/teammanagers/common/payload/code/status/ErrorStatus.java
+++ src/main/java/kr/teammanagers/common/payload/code/status/ErrorStatus.java
@@ -17,7 +17,6 @@ public enum ErrorStatus implements ErrorBaseCode {
     _FORBIDDEN(HttpStatus.FORBIDDEN, "403", "금지된 요청입니다."),
     _NOT_FOUND(HttpStatus.NOT_FOUND, "404", "찾을 수 없습니다."),
 
-
     // 팀 관련 응답
     TEAM_NOT_FOUND(HttpStatus.NOT_FOUND, "TEAM404", "존재하지 않는 팀입니다."),
     TEAM_CONFLICT(HttpStatus.CONFLICT, "TEAM409", "이미 가입된 팀입니다."),
@@ -32,6 +31,7 @@ public enum ErrorStatus implements ErrorBaseCode {
     // 멤버 관련 응답
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER404", "존재하지 않는 유저입니다."),
     MEMBER_SOCIAL_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER4041", "존재하지 않는 소셜 타입입니다."),
+    MEMBER_COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER4042", "존재하지 않는 코멘트입니다."),
 
     // 일정 관련 응답
     CALENDAR_NOT_FOUND(HttpStatus.NOT_FOUND, "CALENDAR404", "존재하지 않는 일정입니다."),
@@ -45,6 +45,10 @@ public enum ErrorStatus implements ErrorBaseCode {
     // 메모 관련 응답
     MEMO_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMO404", "존재하지 않는 메모입니다."),
 
+    // 태그 관련 응답
+    TAG_NOT_FOUND(HttpStatus.NOT_FOUND, "TAG404", "존재하지 않는 태그입니다."),
+    TAG_TEAM_NOT_FOUND(HttpStatus.NOT_FOUND, "TAG4041", "존재하지 않는 팀 태그입니다."),
+    TAG_ROLE_NOT_FOUND(HttpStatus.NOT_FOUND, "TAG4042", "존재하지 않는 역할 태그입니다."),
     ;
 
     private final HttpStatus httpStatus;
