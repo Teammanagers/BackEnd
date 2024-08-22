92e05d2a76446fa9007a1f1beb953ef38394d97b
Luke_YS <yjsmk0902@gmail.com>
2024-08-22 23:25:34 +0900

    Refactor: 캘린더 도메인 모듈서비스 계층 분리 적용 (#104)

--- src/main/java/kr/teammanagers/calendar/repository/TeamCalendarRepository.java
+++ src/main/java/kr/teammanagers/calendar/repository/TeamCalendarRepository.java
@@ -1,7 +1,6 @@
 package kr.teammanagers.calendar.repository;
 
 import kr.teammanagers.calendar.domain.TeamCalendar;
-import kr.teammanagers.team.domain.TeamManage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,4 +11,5 @@ public interface TeamCalendarRepository extends JpaRepository<TeamCalendar, Long
     Optional<TeamCalendar> findByCalendarIdAndTeamManageId(Long calendarId, Long teamManageId);
     List<TeamCalendar> findAllByTeamManageId(Long teamManageId);
     void deleteAllByCalendarId(Long calendarId);
+    void deleteAllByTeamManageId(Long teamManageId);
 }
