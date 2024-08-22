5816167f20282add7049c0faac016a478bb0d307
지현수 <itip1@mju.ac.kr>
2024-08-22 21:29:33 +0900

    [FIX] storage user authentication fix

--- src/main/java/kr/teammanagers/storage/application/StorageCommandService.java
+++ src/main/java/kr/teammanagers/storage/application/StorageCommandService.java
@@ -1,10 +1,10 @@
 package kr.teammanagers.storage.application;
 
+import kr.teammanagers.member.domain.Member;
 import kr.teammanagers.storage.dto.request.CreateStorageRequest;
 import kr.teammanagers.storage.dto.response.StorageResponse;
-import org.springframework.security.core.userdetails.User;
 
 public interface StorageCommandService {
-    StorageResponse uploadFile(CreateStorageRequest request, User user);
-    void deleteFile(Long teamId, Long storageId, User user);
+    StorageResponse uploadFile(CreateStorageRequest request, Member member);
+    void deleteFile(Long teamId, Long storageId, Member member);
 }
