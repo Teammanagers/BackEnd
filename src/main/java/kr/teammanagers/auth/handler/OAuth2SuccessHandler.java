05abbce11a368c647c36a70f2cb47e7feb942446
JiHyeonSu <itip1@mju.ac.kr>
2024-08-23 01:18:23 +0900

    fix redirect Url

--- src/main/java/kr/teammanagers/auth/handler/OAuth2SuccessHandler.java
+++ src/main/java/kr/teammanagers/auth/handler/OAuth2SuccessHandler.java
@@ -27,7 +27,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         String accessToken = tokenProvider.generateAccessToken(authentication);
         tokenProvider.generateRefreshToken(authentication, accessToken);
 
-        String redirectUrl = "https://www.teammanagers.kr" + "/login";
+        String redirectUrl = "http://localhost:5173" + "/login";
 
         redirectUrl = UriComponentsBuilder.fromUriString(redirectUrl)
                 .queryParam("accessToken", accessToken)
